b86c74e47e95b78d8b018168da3f53a0ed0b67d4
Corentin Clichy <cor.clichy@gmail.com>
2021-08-02 09:32:09 -0400

    lint js file

--- src/js/utils/test.js
+++ src/js/utils/test.js
@@ -1,9 +1,9 @@
-import Search from "../data/Search";
+import Search from '../data/Search';
 
 const search = new Search();
 
-const KeywordTest1 = ["coulis", "tomate"];
+const KeywordTest1 = ['coulis', 'tomate'];
 
-const KeywordTest2 = ["fdsfds", "tomate"];
+const KeywordTest2 = ['fdsfds', 'tomate'];
 
 // export default test
